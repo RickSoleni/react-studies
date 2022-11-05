10c6a5e7d0b9e0e030887431566871528a091ab4
Ricardo Giantomaso <ricardo_tech@live.com>
2022-11-05 15:12:53 -0300

    Altering basic React/JSX tags For studies, I altered JSX tags on App.js, loading image and changed page's tittle

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Rick"
+  const url = "https://img.ifunny.co/images/0ac9a157aa48d2c3e7c120b05d849a00a37764f485549ebe832712af1160dd4c_1.webp"
+  function soma(a,b){
+    return a+b
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <h2>Alterando JSX</h2>
+        </div>
+        <div><h3>Testando a constante de nome: {name}</h3></div>
+        <p> Testando função "function soma(a,b)": {soma(1,5)}</p>
+        <img src={url} alt="Minha imagem" />
       </header>
     </div>
   );
