09835546da347c545abc4ae3c445decef1d72d83
Ricardo Giantomaso <ricardo_tech@live.com>
2022-11-06 13:12:20 -0300

    Add CSS Module for Frase components ADD CSS Module and testing Frase component style

--- meu-projeto/src/components/Pessoa.js
+++ meu-projeto/src/components/Pessoa.js
@@ -2,7 +2,6 @@ function Pessoa ({nome, idade, profissao, foto})
 {
     return (
         <div>
-            
             <h2> Nome: {nome}</h2>
             <p>Idade: {idade}</p>
             <p>Profissão: {profissao}</p>
